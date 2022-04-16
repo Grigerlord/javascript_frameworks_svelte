90d63f3839dcf9d3fb1a8c219dcc40df156c043c
Griger <87385460+Grigerlord@users.noreply.github.com>
2022-04-15 20:15:40 -0400

    Initial commit

---
+++ Form.svelte
@@ -0,0 +1,29 @@
+<script>
+  const movies = [
+    {
+      name: "Advengers",
+      available: 3,
+      quantity: 0
+    },
+    {
+      name: "Titanic",
+      available: 5,
+      quantity: 0
+    },
+    {
+      name: "Terminator",
+      available: 4,
+      quantity: 0
+    }
+  ];
+</script>
+
+
+{#each movies as movie}
+  <form>
+    <h3>{movie.name}</h3>
+    <button type="button" on:click={() => movie.quantity -= 1} disabled={movie.quantity <= 0} > - </button>
+    {movie.quantity}
+    <button type="button" on:click={() => movie.quantity += 1} disabled={movie.quantity >= movie.available} > + </button>
+  </form>
+{/each}
